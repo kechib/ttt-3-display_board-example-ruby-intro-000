d57476fc3342e016757823a0c7202454a74ac26d
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-20 17:54:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board = "x"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+def display_board
+  puts "A Tic Tac Toe Board"
+end 
