78312867ff67c9a9f4f7db1deee1069e6f1476e9
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-20 17:55:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
 def display_board
   puts "A Tic Tac Toe Board"
-end 
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
